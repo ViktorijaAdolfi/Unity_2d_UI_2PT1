28bd125c1f9f90aa9b23a2b63c9a034eb05a7c00
Bugsters104 <kristaps@lvt.lv>
2024-05-09 09:21:14 +0300

    Izveidots pirmais skripts

---
+++ Assets/Script/ShowTextScrip.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowTextScrip : MonoBehaviour
+{
+    /*
+     // Start is called before the first frame update
+     void Start()
+     {
+         Debug.Log("Uzstartēts!"); 
+     }
+
+     public int kadrs = 0;
+     // Update is called once per frame
+     void Update()
+     {
+         kadrs++;
+         Debug.Log(kadrs);
+     }
+    */
+
+    string text;
+    public GameObject inputField;
+    public GameObject displayField;
+
+
+    //Šeit papildināsim ar Random
+    public void getText()
+    {
+        text = inputField.GetComponent<Text>().text;
+        displayField.GetComponent<Text>().text = "Sveiks " + text.ToLower() + "!";
+    }
+
+
+}
